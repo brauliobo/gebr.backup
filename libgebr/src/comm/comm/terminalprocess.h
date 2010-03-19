041d6d802ad2678a5c898995bbbd536122913854
Ian Liu Rodrigues <ian.liu88@gmail.com>
2010-03-19 13:03:36 -0300

    Fixed bug when running unconfigured flows
    When a flow has only unconfigured programs, G?BRd returned an empty queue to
    G?BR. This caused a bug where the job was put into a funky queue. Now, G?BRd
    returns a queue named 'j0', conforming with the new naming scheme and solves
    the bug.
    
    Also, various IO functions which require inspecting its return value are now
    checked, displaying warning messages with useful informations on how to debug
    it.

---
+++ libgebr/src/comm/comm/terminalprocess.h
@@ -0,0 +1,97 @@
+/*   libgebr - GeBR Library
+ *   Copyright (C) 2007-2009 GeBR core team (http://www.gebrproject.com/)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *   Partly inspired on Qt 4.3 version of QProcess, by Trolltech
+ */
+
+#ifndef __GEBR_COMM_TERMINAL_PROCESS_H
+#define __GEBR_COMM_TERMINAL_PROCESS_H
+
+#include <glib.h>
+#include <glib-object.h>
+#include <netinet/in.h>
+
+typedef struct _GebrCommTerminalProcess GebrCommTerminalProcess;
+typedef struct _GebrCommTerminalProcessClass GebrCommTerminalProcessClass;
+
+G_BEGIN_DECLS GType gebr_comm_terminal_process_get_type(void);
+
+#define GEBR_COMM_TERMINAL_PROCESS_TYPE			(gebr_comm_terminal_process_get_type())
+#define GEBR_COMM_TERMINAL_PROCESS(obj)			(G_TYPE_CHECK_INSTANCE_CAST ((obj), GEBR_COMM_TERMINAL_PROCESS_TYPE, GebrCommTerminalProcess))
+#define GEBR_COMM_TERMINAL_PROCESS_CLASS(klass)		(G_TYPE_CHECK_CLASS_CAST ((klass), GEBR_COMM_TERMINAL_PROCESS_TYPE, GebrCommTerminalProcessClass))
+#define GEBR_COMM_IS_TERMINAL_PROCESS(obj)		(G_TYPE_CHECK_INSTANCE_TYPE ((obj), GEBR_COMM_TERMINAL_PROCESS_TYPE))
+#define GEBR_COMM_IS_TERMINAL_PROCESS_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE ((klass), GEBR_COMM_TERMINAL_PROCESS_TYPE))
+#define GEBR_COMM_TERMINAL_PROCESS_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS ((obj), GEBR_COMM_TERMINAL_PROCESS_TYPE, GebrCommTerminalProcessClass))
+
+enum GebrCommTerminalProcessExitStatus {
+	GEBR_COMM_TERMINAL_PROCESS_NORMAL_EXIT,
+	GEBR_COMM_TERMINAL_PROCESS_CRASH_EXIT
+};
+
+struct _GebrCommTerminalProcess {
+	GObject parent;
+
+	GPid pid;
+	gboolean is_running;
+	gint exit_code;
+	enum GebrCommTerminalProcessExitStatus exit_status;
+
+	GIOChannel *ptm_io_channel;
+	guint ptm_watch_id;
+	guint finish_watch_id;
+};
+struct _GebrCommTerminalProcessClass {
+	GObjectClass parent;
+
+	/* signals */
+	void (*ready_read) (GebrCommTerminalProcess * self);
+	void (*finished) (GebrCommTerminalProcess * self);
+};
+
+/*
+ * user functions
+ */
+
+GebrCommTerminalProcess *gebr_comm_terminal_process_new(void);
+
+void gebr_comm_terminal_process_free(GebrCommTerminalProcess *);
+
+gboolean gebr_comm_terminal_process_is_running(GebrCommTerminalProcess *);
+
+gboolean gebr_comm_terminal_process_start(GebrCommTerminalProcess *, GString *);
+
+GPid gebr_comm_terminal_process_get_pid(GebrCommTerminalProcess *);
+
+void gebr_comm_terminal_process_kill(GebrCommTerminalProcess *);
+
+void gebr_comm_terminal_process_terminate(GebrCommTerminalProcess *);
+
+gulong gebr_comm_terminal_process_bytes_available(GebrCommTerminalProcess *);
+
+GByteArray *gebr_comm_terminal_process_read(GebrCommTerminalProcess *, gsize);
+
+GString *gebr_comm_terminal_process_read_string(GebrCommTerminalProcess *, gsize);
+
+GByteArray *gebr_comm_terminal_process_read_all(GebrCommTerminalProcess *);
+
+GString *gebr_comm_terminal_process_read_string_all(GebrCommTerminalProcess *);
+
+gsize gebr_comm_terminal_process_write(GebrCommTerminalProcess *, GByteArray *);
+
+gsize gebr_comm_terminal_process_write_string(GebrCommTerminalProcess *, GString *);
+
+G_END_DECLS
+#endif				//__GEBR_COMM_TERMINAL_PROCESS_H
---
+++ libgebr/src/geoxml/geoxml/parameters.c
@@ -0,0 +1,255 @@
+/*   libgebr - GeBR Library
+ *   Copyright (C) 2007-2009 GeBR core team (http://www.gebrproject.com/)
+ *
+ *   This parameters is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This parameters is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this parameters.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <gdome.h>
+
+#include "parameters.h"
+#include "xml.h"
+#include "types.h"
+#include "error.h"
+#include "program_parameter.h"
+#include "program_parameter_p.h"
+#include "parameter_p.h"
+#include "parameter_group.h"
+
+/*
+ * internal stuff
+ */
+
+struct gebr_geoxml_parameters {
+	GdomeElement *element;
+};
+
+GebrGeoXmlParameters *__gebr_geoxml_parameters_append_new(GdomeElement * parent)
+{
+	GebrGeoXmlParameters *parameters;
+
+	parameters = (GebrGeoXmlParameters *) __gebr_geoxml_insert_new_element(parent, "parameters", NULL);
+	gebr_geoxml_parameters_set_exclusive(parameters, NULL);
+
+	return parameters;
+}
+
+gboolean __gebr_geoxml_parameters_group_check(GebrGeoXmlParameters * parameters)
+{
+	GdomeElement *parent_element;
+
+	/* if this is not in a group then there is no problem */
+	parent_element = (GdomeElement *) gdome_el_parentNode((GdomeElement *) parameters, &exception);
+	if (strcmp(gdome_el_nodeName(parent_element, &exception)->str, "group") != 0)
+		return TRUE;
+
+	return (gboolean) (__gebr_geoxml_get_element_index((GdomeElement *) parameters) == 0);
+}
+
+void
+__gebr_geoxml_parameters_do_insert_in_group_stuff(GebrGeoXmlParameters * parameters, GebrGeoXmlParameter * parameter)
+{
+	GdomeElement *parent;
+	GebrGeoXmlParameterGroup *parameter_group;
+	glong index;
+	GebrGeoXmlSequence *instance;
+
+	parent = (GdomeElement *) gdome_el_parentNode((GdomeElement *) parameters, &exception);
+	if (strcmp(gdome_el_tagName(parent, &exception)->str, "group"))
+		return;
+
+	index = __gebr_geoxml_get_element_index((GdomeElement *) parameter);
+	parameter_group = GEBR_GEOXML_PARAMETER_GROUP(gdome_el_parentNode(parent, &exception));
+	gebr_geoxml_parameter_group_get_instance(parameter_group, &instance, 1);
+	for (; instance != NULL; gebr_geoxml_sequence_next(&instance)) {
+		GdomeElement *reference;
+		GebrGeoXmlSequence *position;
+
+		reference = (GdomeElement *) gdome_el_cloneNode((GdomeElement *) parameter, TRUE, &exception);
+		__gebr_geoxml_element_assign_new_id(reference, NULL, FALSE);
+		__gebr_geoxml_parameter_set_be_reference((GebrGeoXmlParameter *) reference, parameter);
+
+		gebr_geoxml_parameters_get_parameter(GEBR_GEOXML_PARAMETERS(instance), &position, index);
+		gdome_el_insertBefore_protected((GdomeElement *) instance,
+				      (GdomeNode *) reference, (GdomeNode *) position, &exception);
+	}
+}
+
+/*
+ * library functions.
+ */
+
+GebrGeoXmlParameter *gebr_geoxml_parameters_append_parameter(GebrGeoXmlParameters * parameters,
+							     enum GEBR_GEOXML_PARAMETER_TYPE type)
+{
+	if (parameters == NULL)
+		return NULL;
+	if (type == GEBR_GEOXML_PARAMETER_TYPE_REFERENCE)
+		return NULL;
+	if (__gebr_geoxml_parameters_group_check(parameters) == FALSE)
+		return NULL;
+
+	GdomeElement *element;
+
+	element = __gebr_geoxml_insert_new_element((GdomeElement *) parameters, "parameter", NULL);
+	__gebr_geoxml_element_assign_new_id(element, NULL, FALSE);
+	__gebr_geoxml_insert_new_element(element, "label", NULL);
+	__gebr_geoxml_parameter_insert_type((GebrGeoXmlParameter *) element, type);
+
+	__gebr_geoxml_parameters_do_insert_in_group_stuff(parameters, (GebrGeoXmlParameter *) element);
+
+	return (GebrGeoXmlParameter *) element;
+}
+
+void gebr_geoxml_parameters_set_exclusive(GebrGeoXmlParameters * parameters, GebrGeoXmlParameter * parameter)
+{
+	if (parameters == NULL)
+		return;
+	if (parameter == NULL) {
+		__gebr_geoxml_set_attr_value((GdomeElement *) parameters, "exclusive", "0");
+		return;
+	}
+
+	gchar *value;
+
+	value = g_strdup_printf("%ld", __gebr_geoxml_get_element_index((GdomeElement *) parameter) + 1);
+	__gebr_geoxml_set_attr_value((GdomeElement *) parameters, "exclusive", value);
+	g_free(value);
+
+	if (gebr_geoxml_parameters_get_selected(parameters) == NULL)
+		gebr_geoxml_parameters_set_selected(parameters, parameter);
+}
+
+GebrGeoXmlParameter *gebr_geoxml_parameters_get_exclusive(GebrGeoXmlParameters * parameters)
+{
+	if (parameters == NULL)
+		return FALSE;
+
+	gulong index;
+	GebrGeoXmlSequence *parameter;
+
+	index = atol(__gebr_geoxml_get_attr_value((GdomeElement *) parameters, "exclusive"));
+	if (index == 0)
+		return NULL;
+	index--;
+	gebr_geoxml_parameters_get_parameter(parameters, &parameter, index);
+
+	/* there isn't a parameter at index, so use the first parameter of the group */
+	if (parameter == NULL)
+		return (GebrGeoXmlParameter *) gebr_geoxml_parameters_get_first_parameter(parameters);
+
+	return (GebrGeoXmlParameter *) parameter;
+}
+
+void gebr_geoxml_parameters_set_selected(GebrGeoXmlParameters * parameters, GebrGeoXmlParameter * parameter)
+{
+	if (parameters == NULL)
+		return;
+	if (gebr_geoxml_parameters_get_exclusive(parameters) == NULL)
+		return;
+	if (parameter == NULL) {
+		__gebr_geoxml_set_attr_value((GdomeElement *) parameters, "selected", "0");
+		return;
+	}
+
+	gchar *value;
+
+	value = g_strdup_printf("%ld", __gebr_geoxml_get_element_index((GdomeElement *) parameter) + 1);
+	__gebr_geoxml_set_attr_value((GdomeElement *) parameters, "selected", value);
+	g_free(value);
+}
+
+GebrGeoXmlParameter *gebr_geoxml_parameters_get_selected(GebrGeoXmlParameters * parameters)
+{
+	if (parameters == NULL)
+		return FALSE;
+	if (gebr_geoxml_parameters_get_exclusive(parameters) == NULL)
+		return NULL;
+
+	gulong index;
+	GebrGeoXmlSequence *parameter;
+
+	index = atol(__gebr_geoxml_get_attr_value((GdomeElement *) parameters, "selected"));
+	if (index == 0)
+		return NULL;
+	index--;
+	gebr_geoxml_parameters_get_parameter(parameters, &parameter, index);
+
+	/* there isn't a parameter at index, so use the first parameter of the group */
+	if (parameter == NULL)
+		return (GebrGeoXmlParameter *) gebr_geoxml_parameters_get_first_parameter(parameters);
+
+	return (GebrGeoXmlParameter *) parameter;
+}
+
+GebrGeoXmlSequence *gebr_geoxml_parameters_get_first_parameter(GebrGeoXmlParameters * parameters)
+{
+	if (parameters == NULL)
+		return NULL;
+	return (GebrGeoXmlSequence *) __gebr_geoxml_get_first_element((GdomeElement *) parameters, "parameter");
+}
+
+int
+gebr_geoxml_parameters_get_parameter(GebrGeoXmlParameters * parameters, GebrGeoXmlSequence ** parameter, gulong index)
+{
+	if (parameters == NULL) {
+		*parameter = NULL;
+		return GEBR_GEOXML_RETV_NULL_PTR;
+	}
+
+	*parameter = (GebrGeoXmlSequence *) __gebr_geoxml_get_element_at((GdomeElement *) parameters,
+									 "parameter", index, FALSE);
+
+	return (*parameter == NULL)
+	    ? GEBR_GEOXML_RETV_INVALID_INDEX : GEBR_GEOXML_RETV_SUCCESS;
+}
+
+glong gebr_geoxml_parameters_get_number(GebrGeoXmlParameters * parameters)
+{
+	if (parameters == NULL)
+		return -1;
+	return __gebr_geoxml_get_elements_number((GdomeElement *) parameters, "parameter");
+}
+
+gboolean gebr_geoxml_parameters_get_is_in_group(GebrGeoXmlParameters * parameters)
+{
+	if (parameters == NULL)
+		return FALSE;
+	return !strcmp(gdome_el_tagName((GdomeElement *) gdome_el_parentNode((GdomeElement *) parameters, &exception),
+					&exception)->str, "group")
+	    ? TRUE : FALSE;
+}
+
+GebrGeoXmlParameterGroup *gebr_geoxml_parameters_get_group(GebrGeoXmlParameters * parameters)
+{
+	if (parameters == NULL)
+		return NULL;
+	GdomeElement *parent_element;
+	return !strcmp(gdome_el_tagName((parent_element = (GdomeElement *) gdome_el_parentNode((GdomeElement *)
+											       parameters, &exception)),
+					&exception)->str, "group")
+	    ? (GebrGeoXmlParameterGroup *) gdome_el_parentNode(parent_element, &exception)
+	    : NULL;
+}
+
+void gebr_geoxml_parameters_reset(GebrGeoXmlParameters * parameters, gboolean recursive)
+{
+	GebrGeoXmlSequence *parameter;
+
+	parameter = gebr_geoxml_parameters_get_first_parameter(parameters);
+	for (; parameter != NULL; gebr_geoxml_sequence_next(&parameter))
+		gebr_geoxml_parameter_reset(GEBR_GEOXML_PARAMETER(parameter), recursive);
+}
