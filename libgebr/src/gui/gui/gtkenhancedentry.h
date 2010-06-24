337b5dc7928877b9dc744c57da85d76c6e417366
Alexandre Baaklini Gomes Coelho <abaaklini@gebrproject.com>
2010-06-24 11:00:30 -0300

    Fixed a bug at gebr_geoxml_flow_get_servers_number
    Update issue 444
    The function gebr_geoxml_flow_get_servers_number was returning -1 for any flow.

---
+++ libgebr/src/gui/gui/gtkenhancedentry.h
@@ -0,0 +1,60 @@
+/*   libgebr - GeBR Library
+ *   Copyright (C) 2007-2009 GeBR core team (http://www.gebrproject.com/)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GEBR_GUI_GTK_ENHANCED_ENTRY_H
+#define __GEBR_GUI_GTK_ENHANCED_ENTRY_H
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+GType gebr_gui_gtk_enhanced_entry_get_type(void);
+
+#define GEBR_GUI_GTK_TYPE_ENHANCED_ENTRY		(gebr_gui_gtk_enhanced_entry_get_type())
+#define GEBR_GUI_GTK_ENHANCED_ENTRY(obj)		(G_TYPE_CHECK_INSTANCE_CAST ((obj), GEBR_GUI_GTK_TYPE_ENHANCED_ENTRY, GebrGuiGtkEnhancedEntry))
+#define GEBR_GUI_GTK_ENHANCED_ENTRY_CLASS(klass)	(G_TYPE_CHECK_CLASS_CAST ((klass), GEBR_GUI_GTK_TYPE_ENHANCED_ENTRY, GebrGuiGtkEnhancedEntryClass))
+#define GEBR_GUI_GTK_IS_ENHANCED_ENTRY(obj)		(G_TYPE_CHECK_INSTANCE_TYPE ((obj), GEBR_GUI_GTK_TYPE_ENHANCED_ENTRY))
+#define GEBR_GUI_GTK_IS_ENHANCED_ENTRY_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE ((klass), GEBR_GUI_GTK_TYPE_ENHANCED_ENTRY))
+#define GEBR_GUI_GTK_ENHANCED_ENTRY_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS ((obj), GEBR_GUI_GTK_TYPE_ENHANCED_ENTRY, GebrGuiGtkEnhancedEntryClass))
+
+typedef struct _GebrGuiGtkEnhancedEntry GebrGuiGtkEnhancedEntry;
+typedef struct _GebrGuiGtkEnhancedEntryClass GebrGuiGtkEnhancedEntryClass;
+
+struct _GebrGuiGtkEnhancedEntry {
+	GtkEntry parent;
+
+	gboolean empty;
+	gchar *empty_text;
+};
+struct _GebrGuiGtkEnhancedEntryClass {
+	GtkEntryClass parent;
+};
+
+GtkWidget *gebr_gui_gtk_enhanced_entry_new();
+
+GtkWidget *gebr_gui_gtk_enhanced_entry_new_with_empty_text(const gchar * empty_text);
+
+void gebr_gui_gtk_enhanced_entry_set_text(GebrGuiGtkEnhancedEntry * enhanced_entry, const gchar * text);
+
+const gchar *gebr_gui_gtk_enhanced_entry_get_text(GebrGuiGtkEnhancedEntry * enhanced_entry);
+
+void gebr_gui_gtk_enhanced_entry_set_empty_text(GebrGuiGtkEnhancedEntry * enhanced_entry, const gchar * empty_text);
+
+const gchar *gebr_gui_gtk_enhanced_entry_get_empty_text(GebrGuiGtkEnhancedEntry * enhanced_entry);
+
+G_END_DECLS
+#endif				//__GEBR_GUI_GTK_ENHANCED_ENTRY_H
---
+++ libgebr/src/libgebr.c
@@ -0,0 +1,29 @@
+/*   GeBR Library - Common stuff used by GeBR programs
+ *   Copyright (C) 2007-2009 GeBR core team (http://www.gebrproject.com/)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <locale.h>
+#ifdef ENABLE_NLS
+#	include <libintl.h>
+#endif
+
+#include "libgebr.h"
+
+void gebr_libinit(void)
+{
+	bindtextdomain(GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+	bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
+}
