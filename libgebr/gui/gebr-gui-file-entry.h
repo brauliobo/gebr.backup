1ff47d43932ceee50a248801f2321be59667e54e
Priscila Moraes de Souza <pris.moraess@gebrproject.com>
2011-02-01 12:41:37 -0200

    Added sections about flow queues
    Uptdate issue 825
    
    Added sections about flow queues

---
+++ libgebr/gui/gebr-gui-file-entry.h
@@ -0,0 +1,87 @@
+/*   libgebr - GeBR Library
+ *   Copyright (C) 2007-2009 GeBR core team (http://www.gebrproject.com/)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GEBR_GUI_FILE_ENTRY_H
+#define __GEBR_GUI_FILE_ENTRY_H
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+GType gebr_gui_file_entry_get_type(void);
+
+#define GEBR_GUI_TYPE_FILE_ENTRY		(gebr_gui_file_entry_get_type())
+#define GEBR_GUI_FILE_ENTRY(obj)		(G_TYPE_CHECK_INSTANCE_CAST ((obj), GEBR_GUI_TYPE_FILE_ENTRY, GebrGuiFileEntry))
+#define GEBR_GUI_FILE_ENTRY_CLASS(klass)	(G_TYPE_CHECK_CLASS_CAST ((klass), GEBR_GUI_TYPE_FILE_ENTRY, GebrGuiFileEntryClass))
+#define GEBR_GUI_IS_FILE_ENTRY(obj)		(G_TYPE_CHECK_INSTANCE_TYPE ((obj), GEBR_GUI_TYPE_FILE_ENTRY))
+#define GEBR_GUI_IS_FILE_ENTRY_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE ((klass), GEBR_GUI_TYPE_FILE_ENTRY))
+#define GEBR_GUI_FILE_ENTRY_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS ((obj), GEBR_GUI_TYPE_FILE_ENTRY, GebrGuiFileEntryClass))
+
+typedef struct _GebrGuiFileEntry GebrGuiFileEntry;
+typedef struct _GebrGuiFileEntryClass GebrGuiFileEntryClass;
+
+typedef void (*GebrGuiFileEntryCustomize) (GtkFileChooser *, gpointer);
+
+struct _GebrGuiFileEntry {
+	GtkHBox parent;
+
+	GdkWindow *event_window;
+	GtkWidget *entry;
+
+	gboolean choose_directory;
+	gboolean do_overwrite_confirmation;
+
+	GebrGuiFileEntryCustomize customize_function;
+	gpointer customize_user_data;
+};
+struct _GebrGuiFileEntryClass {
+	GtkHBoxClass parent;
+
+	/* signals */
+	void (*path_changed) (GebrGuiFileEntry * self);
+};
+
+GtkWidget *gebr_gui_file_entry_new(GebrGuiFileEntryCustomize customize_function, gpointer user_data);
+
+void gebr_gui_file_entry_set_choose_directory(GebrGuiFileEntry * file_entry, gboolean choose_directory);
+
+gboolean gebr_gui_file_entry_get_choose_directory(GebrGuiFileEntry * file_entry);
+
+void
+
+
+gebr_gui_file_entry_set_do_overwrite_confirmation(GebrGuiFileEntry * file_entry,
+						      gboolean do_overwrite_confirmation);
+
+gboolean gebr_gui_file_entry_get_do_overwrite_confirmation(GebrGuiFileEntry * file_entry);
+
+void gebr_gui_file_entry_set_path(GebrGuiFileEntry * file_entry, const gchar * path);
+
+const gchar *gebr_gui_file_entry_get_path(GebrGuiFileEntry * file_entry);
+
+/**
+ * gebr_gui_file_entry_set_activates_default:
+ * @self:
+ * @setting:
+ *
+ * Sets this entry to activate the default response for the toplevel window containing @self based on the value of
+ * @setting.
+ */
+void gebr_gui_file_entry_set_activates_default (GebrGuiFileEntry * self, gboolean setting);
+
+G_END_DECLS
+#endif				//__GEBR_GUI_FILE_ENTRY_H
---
+++ libgebr/gui/gebr-gui-program-edit.h
@@ -0,0 +1,70 @@
+/*   libgebr - GeBR Library
+ *   Copyright (C) 2007-2009 GeBR core team (http://www.gebrproject.com/)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GEBR_GUI_PROGRAM_EDIT_H
+#define __GEBR_GUI_PROGRAM_EDIT_H
+
+/**
+ * \file gebr-gui-program-edit.h
+ */
+
+#include <gtk/gtk.h>
+
+#include <geoxml.h>
+
+G_BEGIN_DECLS
+
+typedef struct {
+	GebrGeoXmlProgram *program;
+
+	GtkWidget *widget;
+	gpointer parameter_widget_data;
+	gboolean use_default;
+
+	GtkWidget *scrolled_window;
+	GtkWidget *title_label;
+	GtkWidget *hbox;
+
+	struct gebr_gui_gebr_gui_program_edit_dicts {
+		GebrGeoXmlDocument *project;
+		GebrGeoXmlDocument *line;
+		GebrGeoXmlDocument *flow;
+	} dicts;
+} GebrGuiProgramEdit;
+
+/**
+ * Setup UI for \p program.
+ */
+GebrGuiProgramEdit *
+gebr_gui_program_edit_setup_ui(GebrGeoXmlProgram * program, gpointer parameter_widget_data, gboolean use_default);
+
+/**
+ * \internal
+ * Just free.
+ */
+void gebr_gui_program_edit_destroy(GebrGuiProgramEdit *program_edit);
+
+/**
+ * \internal
+ *
+ * Reload UI of \p gebr_gui_program_edit.
+ * \param program If not NULL, then use it as new program.
+ */
+void gebr_gui_program_edit_reload(GebrGuiProgramEdit *program_edit, GebrGeoXmlProgram * program);
+
+G_END_DECLS
+#endif				//__GEBR_GUI_PROGRAM_EDIT_H
