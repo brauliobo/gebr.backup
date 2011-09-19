b391f681baa13c549adef60050cb1a887152fd1c
Ian Liu Rodrigues <ian.liu@gebrproject.com>
2011-09-19 17:04:41 -0300

    Fix regression when cleaning up configure.ac

---
+++ gebrd/gebrd.h
@@ -0,0 +1,164 @@
+/*   GeBR Daemon - Process and control execution of flows
+ *   Copyright (C) 2007-2009 GeBR core team (http://www.gebrproject.com/)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GEBRD_H
+#define __GEBRD_H
+
+#include <libgebr/comm/gebr-comm-listensocket.h>
+#include <libgebr/comm/gebr-comm-server.h>
+#include <libgebr/log.h>
+#include <libgebr/comm/gebr-comm-server.h>
+#include <libgebr/gebr-validator.h>
+
+#include "gebrd-mpi-interface.h"
+#include "gebrd-user.h"
+
+G_BEGIN_DECLS
+
+GType gebrd_app_get_type(void);
+#define GEBRD_APP_TYPE		(gebrd_app_get_type())
+#define GEBRD_APP(obj)		(G_TYPE_CHECK_INSTANCE_CAST ((obj), GEBRD_APP_TYPE, GebrdApp))
+#define GEBRD_APP_CLASS(klass)	(G_TYPE_CHECK_CLASS_CAST ((klass), GEBRD_APP_TYPE, GebrdAppClass))
+#define GEBRD_IS_APP(obj)		(G_TYPE_CHECK_INSTANCE_TYPE ((obj), GEBRD_APP_TYPE))
+#define GEBRD_IS_APP_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE ((klass), GEBRD_APP_TYPE))
+#define GEBRD_APP_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS ((obj), GEBRD_APP_TYPE, GebrdAppClass))
+
+typedef struct _GebrdApp GebrdApp;
+typedef struct _GebrdAppClass GebrdAppClass;
+
+/**
+ * \file gebrd.h
+ * \brief Global access variable for date interchange 
+ */
+extern GebrdApp *gebrd;
+/**
+ * \struct gebrd gebr.h
+ * \brief Global access variable for date interchange 
+ * \see gebrd.h
+ */
+struct _GebrdApp {
+	GObject parent;
+
+	GebrCommListenSocket *listen_socket;
+	GList *clients;
+	GList * mpi_flavors;
+	GebrdUser *user;
+	GString *user_data_filename;
+
+	gchar hostname[256];
+	GebrCommSocketAddress socket_address;
+	/**
+	 * Options passed through command line
+	 */
+	struct gebrd_options {
+		gboolean foreground;
+	} options;
+
+	GString *run_filename;
+	GString *fs_lock;
+	GebrLog *log;
+	GMainLoop *main_loop;
+	int finished_starting_pipe[2];
+
+	/**
+	 * Validator for flow variables
+	 */
+	GebrGeoXmlDocument *flow;
+	GebrGeoXmlDocument *line;
+	GebrGeoXmlDocument *proj;
+	GebrValidator *validator;
+
+	gint nprocs;
+};
+
+struct _GebrdAppClass {
+	GObjectClass parent;
+};
+
+GebrdApp *gebrd_app_new(void);
+
+/**
+ * Init daemon routines.
+ * Called at #main.c
+ */
+void gebrd_init(void);
+
+/**
+ * Called to exit glib main loop and exit program.
+ */
+void gebrd_quit(void);
+
+/**
+ * Log a message in the gebrd log file and to standand output.
+ * The message in only shown in the stdout if gebrd is running in interative mode.
+ */
+void gebrd_message(GebrLogMessageType type, const gchar * message, ...);
+
+/**
+ * Return a free port to be used for X11 redirection.
+ * The server doesn't listen to it; the client is supposed to
+ * do a port forward thought SSH
+ */
+guint8 gebrd_get_x11_redirect_display(void);
+
+/**
+ * Check wheter running on a Moab cluster.
+ */
+GebrCommServerType gebrd_get_server_type(void);
+
+/**
+ * Loads gebrd configuration file into gebrd configuration structure.
+ * \see gebrd.config
+ */
+void gebrd_config_load(void);
+
+/**
+ */
+void gebrd_user_data_save(void);
+
+/**
+ * TODO
+ */
+const GebrdMpiConfig * gebrd_get_mpi_config_by_name(const gchar * name);
+
+/**
+ * gebrd_clean_proj_line_dicts:
+ *
+ * Clean the dictionary variables from #gebrd.line and #gebrd.proj
+ */
+void gebrd_clean_proj_line_dicts(void);
+
+/*
+ *  gebrd_get_validator:
+ *
+ *  Get validator to gebrd if self->validator != NULL
+ */
+GebrValidator *gebrd_get_validator(GebrdApp *self);
+
+/**
+ * gebrd_app_set_heuristic_aggression:
+ *
+ * @self: The #GebrdApp with number of procs.
+ * @aggressive: Aggression will be the basis of heuristic.
+ * @nice: The nice to be used on processor.
+ *
+ * Returns: The number of procs and nice after set heuristic.
+ */
+gint gebrd_app_set_heuristic_aggression(GebrdApp *self, gint aggressive, gint *nice);
+
+G_END_DECLS
+#endif				//__GEBRD_H
---
+++ libgebr/comm/gebr-comm-protocol.h
@@ -0,0 +1,86 @@
+/*   libgebr - GeBR Library
+ *   Copyright (C) 2007-2009 GeBR core team (http://www.gebrproject.com/)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GEBR_COMM_PROTOCOL_H
+#define __GEBR_COMM_PROTOCOL_H
+
+#include <glib.h>
+
+#include "gebr-comm-streamsocket.h"
+
+G_BEGIN_DECLS
+
+/* DEPRECATED */
+
+extern struct gebr_comm_protocol_defs gebr_comm_protocol_defs;
+#define PROTOCOL_VERSION "1.0.7"
+
+struct gebr_comm_message_def {
+	guint		code_hash;
+	const gchar *	code;
+	gboolean	returns; // does this message send return (RET) command?
+	gint		arg_number;
+};
+
+struct gebr_comm_protocol_defs {
+	GHashTable *hash_table;
+
+	/* messages identifiers hashes */
+	struct gebr_comm_message_def ret_def;
+	struct gebr_comm_message_def ini_def;
+	struct gebr_comm_message_def err_def;
+	struct gebr_comm_message_def qut_def;
+	struct gebr_comm_message_def lst_def;
+	struct gebr_comm_message_def job_def;
+	struct gebr_comm_message_def run_def;
+	struct gebr_comm_message_def rnq_def;
+	struct gebr_comm_message_def flw_def;
+	struct gebr_comm_message_def clr_def;
+	struct gebr_comm_message_def end_def;
+	struct gebr_comm_message_def kil_def;
+	struct gebr_comm_message_def out_def;
+	struct gebr_comm_message_def sta_def;
+};
+
+struct gebr_comm_message {
+	/* message identifier */
+	guint hash;
+	/* argument size */
+	gsize argument_size;
+	/* message argument (can be an empty string) */
+	GString *argument;
+};
+
+struct gebr_comm_protocol {
+	/* the data being received is forming message(s) */
+	GString *data;
+	struct gebr_comm_message *message;
+	/* received messages to be parsed */
+	GList *messages;
+	/* waiting for return of message with "hash"; 0 if it is not waiting responses */
+	guint waiting_ret_hash;
+	/* logged in with INI and RET? */
+	gboolean logged;
+	/* if we are logged, we received a host name from the peer */
+	GString *hostname;
+};
+
+void gebr_comm_protocol_reset(struct gebr_comm_protocol *protocol);
+void gebr_comm_message_free(struct gebr_comm_message *message);
+
+G_END_DECLS
+#endif				//__GEBR_COMM_PROTOCOL_H
