e0c582e69a1627ade4ad4d62ed24cd51cf81ba6c
Ian Liu Rodrigues <ian.liu@gebrproject.com>
2011-09-29 17:26:33 -0300

    Rename gebr_comm_server_run_config_new to gebr_comm_run_config_new

---
+++ gebr/ui_moab.h
@@ -0,0 +1,29 @@
+/*   GeBR - An environment for seismic processing.
+ *   Copyright (C) 2007-2009 GeBR core team (http://www.gebrproject.com/)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __UI_MOAB_H
+#define __UI_MOAB_H
+
+#include <gtk/gtk.h>
+#include "server.h"
+
+G_BEGIN_DECLS
+
+gboolean moab_setup_ui(gchar ** char_account, GebrServer * server, gboolean parallel_program);
+
+G_END_DECLS
+#endif				/* __UI_MOAB_H */
