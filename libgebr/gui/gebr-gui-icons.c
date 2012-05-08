866dcfb069e00245fb186ac4056847ecaf8e71a4
fmatheus <fmatheus@localhost>
2012-05-08 19:55:54 -0300

    Release 0.17.7: More file chooser fixes.

---
+++ libgebr/gui/gebr-gui-icons.c
@@ -0,0 +1,169 @@
+/*   libgebr - GeBR Library
+ *   Copyright (C) 2007-2009 GeBR core team (http://www.gebrproject.com/)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <fnmatch.h>
+
+#include "gtk/gtk.h"
+
+#include "gebr-gui-icons.h"
+
+/*
+ * Private functions
+ */
+
+/*
+ * Library functions
+ */
+
+void gebr_gui_setup_icons(void)
+{
+	static GtkIconFactory *icon_factory = NULL;
+	static const gchar *sizes[] = {
+		"16x16", "22x22", "48x48",
+		"gebr-theme/16x16", "gebr-theme/22x22", "gebr-theme/48x48", NULL
+	};
+	static const int gtk_icon_sizes[] = {
+		GTK_ICON_SIZE_MENU,
+		GTK_ICON_SIZE_SMALL_TOOLBAR,
+		GTK_ICON_SIZE_LARGE_TOOLBAR
+	};
+	GString *size_path;
+	GString *path;
+	int i;
+
+	if (icon_factory == NULL)
+		icon_factory = gtk_icon_factory_new();
+	else
+		return;
+
+	size_path = g_string_new(NULL);
+	path = g_string_new(NULL);
+
+	for (i = 0; sizes[i] != NULL; ++i) {
+		DIR *dir;
+		struct dirent *size_file;
+
+		g_string_printf(size_path, "%s%s", LIBGEBR_ICONS_DIR "/", sizes[i]);
+		if ((dir = opendir(size_path->str)) == NULL)
+			continue;
+
+		while ((size_file = readdir(dir)) != NULL) {
+			DIR *dir;
+			struct dirent *file;
+
+			g_string_printf(path, "%s/%s", size_path->str, size_file->d_name);
+			if ((dir = opendir(path->str)) == NULL)
+				continue;
+
+			while ((file = readdir(dir)) != NULL) {
+				GtkIconSet *icon_set;
+				GtkIconSource *icon_source;
+				GString *filename;
+				gchar *stock_id;
+
+				if (fnmatch("*.png", file->d_name, 1))
+					continue;
+
+				/* remove ".png" from string */
+				filename = g_string_new(NULL);
+				if (g_path_is_absolute(path->str))
+					g_string_printf(filename, "%s/%s", path->str, file->d_name);
+				else {
+					gchar *cwd = g_get_current_dir();
+					g_string_printf(filename, "%s/%s/%s", cwd, path->str, file->d_name);
+					g_free(cwd);
+				}
+				stock_id = g_strndup(file->d_name, strlen(file->d_name) - 4);
+
+				icon_source = gtk_icon_source_new();
+				gtk_icon_source_set_filename(icon_source, filename->str);
+				gtk_icon_source_set_size(icon_source, (GtkIconSize) gtk_icon_sizes[i]);
+				gtk_icon_source_set_size_wildcarded(icon_source, TRUE);
+				gtk_icon_source_set_direction_wildcarded(icon_source, TRUE);
+				gtk_icon_source_set_state_wildcarded(icon_source, TRUE);
+
+				icon_set = gtk_icon_factory_lookup(icon_factory, stock_id);
+				if (icon_set == NULL)
+					icon_set = gtk_icon_set_new();
+				gtk_icon_set_add_source(icon_set, icon_source);
+				gtk_icon_factory_add(icon_factory, stock_id, icon_set);
+
+				g_string_free(filename, TRUE);
+				g_free(stock_id);
+				gtk_icon_source_free(icon_source);
+			}
+		}
+
+		closedir(dir);
+	}
+
+	/* finally... add icons */
+	gtk_icon_factory_add_default(icon_factory);
+
+	g_string_free(size_path, TRUE);
+	g_string_free(path, TRUE);
+}
+
+const gchar * gebr_gui_get_program_icon(GebrGeoXmlProgram * program)
+{
+	static gchar string[51];
+
+	GebrGeoXmlProgramControl control;
+	GString * temp;
+	const gchar * postfix;
+	GebrGeoXmlProgramStatus status;
+
+	status = gebr_geoxml_program_get_status(program);
+	if (status == GEBR_GEOXML_PROGRAM_STATUS_UNCONFIGURED ||
+	    status == GEBR_GEOXML_PROGRAM_STATUS_UNKNOWN)
+		return GTK_STOCK_DIALOG_WARNING;
+
+	switch(status) {
+	case GEBR_GEOXML_PROGRAM_STATUS_DISABLED:
+		postfix = "disabled";
+		break;
+	case GEBR_GEOXML_PROGRAM_STATUS_CONFIGURED:
+		postfix = "configured";
+		break;
+	default:
+		postfix = "";
+		break;
+	}
+
+	control = gebr_geoxml_program_get_control (program);
+	temp = g_string_new(NULL);
+	if (control == GEBR_GEOXML_PROGRAM_CONTROL_ORDINARY) {
+		g_string_printf(temp, "pipe-%c%c-%s",
+				gebr_geoxml_program_get_stdin(program)? 'o' : 'c',
+				gebr_geoxml_program_get_stdout(program)? 'o' : 'c',
+				postfix);
+	} else
+		g_string_printf (temp, "gebr-loop-%s", postfix);
+
+	strcpy(string, temp->str);
+
+	g_string_free(temp, TRUE);
+	return string;
+}
---
+++ maestro/gebrm-main.c
@@ -0,0 +1,223 @@
+/*
+ * gebrm-main.c
+ * This file is part of GêBR Project
+ *
+ * Copyright (C) 2011 - GêBR Team
+ *
+ * GêBR Project is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GêBR Project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GêBR Project. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <config.h>
+#include "gebrm-app.h"
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <glib.h>
+#include <glib/gstdio.h>
+#include <libgebr/comm/gebr-comm.h>
+#include <libgebr/log.h>
+#include <fcntl.h>
+
+#include "gebrm-app.h"
+
+#include <libgebr/gebr-version.h>
+
+#define GETTEXT_PACKAGE "gebrm"
+
+static gboolean interactive;
+static gboolean show_version;
+static int output_fd = STDOUT_FILENO;
+
+static GOptionEntry entries[] = {
+	{"interactive", 'i', 0, G_OPTION_ARG_NONE, &interactive,
+		"Run server in interactive mode, not as a daemon", NULL},
+	{"version", 'v', 0, G_OPTION_ARG_NONE, &show_version,
+		"Show GeBR daemon version", NULL},
+	{NULL}
+};
+
+void
+fork_and_exit_main(void)
+{
+	int fd[2];
+
+	if (pipe(fd) == -1) {
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+
+	pid_t pid = fork();
+
+	if (pid == (pid_t)-1) {
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+
+	if (pid != 0) {
+		char c;
+		GString *buf = g_string_sized_new(10);
+		while (read(fd[0], &c, 1) > 0) {
+			g_string_append_c(buf, c);
+			if (c == '\n') {
+				g_string_append_c(buf, '\0');
+				break;
+			}
+		}
+		printf("%s", buf->str);
+
+		g_string_free(buf, TRUE);
+		exit(EXIT_SUCCESS);
+	}
+
+	umask(0);
+
+	if (setsid() == (pid_t)-1) {
+		perror("setsid");
+		exit(EXIT_FAILURE);
+	}
+
+	if (chdir("/") == -1) {
+		perror("chdir");
+		exit(EXIT_FAILURE);
+	}
+
+	output_fd = fd[1];
+	int null = open("/dev/null", O_RDWR);
+	(void)dup2(null, STDIN_FILENO);
+	(void)dup2(null, STDOUT_FILENO);
+	(void)dup2(null, STDERR_FILENO);
+
+	/* Gebr opens the file des number 3 when calling
+	 * Maestro's binary. We close it as a workaround
+	 * to the double-connection bug.
+	 */
+	close(3);
+}
+
+static void
+gebrm_remove_lock_and_quit(int sig)
+{
+	g_unlink(gebrm_app_get_lock_file());
+	g_unlink(gebrm_app_get_version_file());
+	//g_debug("On '%s', line '%d', version_file:'%s' ", __FILE__, __LINE__, gebrm_app_get_version_file());
+	exit(0);
+}
+
+int
+main(int argc, char *argv[])
+{
+	GError *error = NULL;
+	GOptionContext *context;
+
+	context = g_option_context_new("GêBR Maestro");
+	g_option_context_add_main_entries(context, entries, GETTEXT_PACKAGE);
+
+	if (!g_option_context_parse(context, &argc, &argv, &error)) {
+		g_print("option parsing failed: %s\n", error->message);
+		exit(EXIT_FAILURE);
+	}
+
+	g_type_init();
+	g_thread_init(NULL);
+
+	if (show_version) {
+		fprintf(stdout, "%s (%s)\n", GEBR_VERSION NANOVERSION, gebr_version());
+		exit(EXIT_SUCCESS);
+	}
+
+	const gchar *lock = gebrm_app_get_lock_file();
+	const gchar *version_file  = gebrm_app_get_version_file();
+	gchar *lock_contents;
+	GError *lock_error = NULL;
+
+	if (g_access(lock, R_OK | W_OK) == 0) { //check lock_file
+		GError *version_error = NULL;
+
+		g_file_get_contents(lock, &lock_contents, NULL, &lock_error);
+		if (lock_error) {
+			if (lock_error)
+				g_critical("Error reading lock/version: %s", lock_error->message);
+			g_free(lock_contents);
+			exit(1);
+		}
+		g_free(lock_error);
+
+		gint port = atoi(lock_contents);
+
+		if (g_access(version_file, R_OK | W_OK) == 0) { //It has the version file
+			gchar *version_contents;
+			g_file_get_contents(version_file, &version_contents, NULL, &version_error);
+			if (version_error) {
+				if (version_error)
+					g_critical("Error reading lock/version: %s", version_error->message);
+				g_free(version_contents);
+				exit(1);
+			}
+			g_free(version_error);
+
+			gchar *curr_version = g_strdup_printf("%s (%s)\n", GEBR_VERSION NANOVERSION, gebr_version());
+
+			if (!gebr_comm_listen_socket_is_local_port_available(port)) {
+				if (g_strcmp0(curr_version, version_contents) == 0) { //It is running in the same version
+					g_print("%s\n", lock_contents);
+					exit(0);
+				} else {		//It is running in a different version
+					gebr_kill_by_port(port);
+				}
+			}
+			g_free(curr_version);
+			g_free(version_contents);
+		} else {		//It does not have version file
+			gebr_kill_by_port(port);
+		}
+		g_free(lock_contents);
+	} else if (g_access(lock, F_OK) == 0) {
+		g_critical("Cannot read/write into %s", lock);
+		exit(1);
+	}
+
+	if (!interactive)
+		fork_and_exit_main();
+
+	struct sigaction sa;
+	sa.sa_handler = gebrm_remove_lock_and_quit;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+
+	if (sigaction(SIGTERM, &sa, NULL)
+	    || sigaction(SIGINT, &sa, NULL)
+	    || sigaction(SIGQUIT, &sa, NULL)
+	    || sigaction(SIGSEGV, &sa, NULL))
+		perror("sigaction");
+
+	gebr_geoxml_init();
+
+	gchar *path = g_build_filename(g_get_home_dir(), ".gebr", "gebrm",
+				       g_get_host_name(), "log", NULL);
+	gebr_log_set_default(path);
+	g_free(path);
+
+	GebrmApp *app = gebrm_app_singleton_get();
+
+	if (!gebrm_app_run(app, output_fd))
+		exit(EXIT_FAILURE);
+
+	g_object_unref(app);
+	gebr_geoxml_finalize();
+
+	return EXIT_SUCCESS;
+}
