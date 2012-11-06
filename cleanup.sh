3194b35a109319656fa7ba75b9a8a19819ed61f4
Ian Liu Rodrigues <ian.liu88@gmail.com>
2012-11-06 10:54:41 -0200

    Add GebrCommPortForward in ::port-defined signal

---
+++ cleanup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+AUTH="$HOME/.ssh/authorized_keys"
+shopt -s nullglob
+shopt -s extglob
+signal=${2--15}
+
+function killg() {
+echo Killing $1...
+for lock in $HOME/.gebr/$1/*/lock; do
+    host=`basename ${lock%lock}`;
+    echo -en "$host\t"
+    ssh $host killall -q mpich-hello \; fuser -sk $signal '$(cat '$lock')/tcp'
+    if [ $? -eq 1 ]; then
+	echo -e "CRASH"; rm -rf $lock
+    else
+	echo -e "OK"
+    fi
+done
+}
+
+sync
+
+if [ -f $AUTH.cleanup ]; then
+	[ -f $AUTH ] && mv $AUTH $AUTH.cleanup.tmp
+	mv $AUTH.cleanup $AUTH
+	UNDO_AUTH=1
+fi
+
+case x$1 in
+    xgebrd | xd)
+      killg gebrd
+      ;;
+    xgebrm | xm)
+      killg gebrm
+      ;;
+    x-h | xhelp)
+      echo "Usage: [-h|gebrd|gebrm|all|ALL]"
+      ;;
+    xall | x)
+      killg gebrd
+      killg gebrm
+      killg gebr-comm-socketchannel
+      ;;
+    xALL)
+      killg gebrd
+      killg gebrm
+      [ -d ~/.gebr ] && mv ~/.gebr ~/.gebr-`date +%Y%m%d-%H%M%S`
+      ;;
+esac
+
+if [ $UNDO_AUTH ]; then
+	mv $AUTH $AUTH.cleanup
+	[ -f $AUTH.cleanup.tmp ] && mv $AUTH.cleanup.tmp $AUTH
+fi
---
+++ gebr/gebr-menu-view.h
@@ -0,0 +1,131 @@
+/*
+ * gebr-menu-view.h
+ * This file is part of GêBR Project
+ *
+ * Copyright (C) 2012 - GêBR core team (www.gebrproject.com)
+ *
+ * GêBR Project is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GêBR Project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GêBR Project. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GEBR_MENU_VIEW_H__
+#define __GEBR_MENU_VIEW_H__
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define GEBR_TYPE_MENU_VIEW            (gebr_menu_view_get_type ())
+#define GEBR_MENU_VIEW(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GEBR_TYPE_MENU_VIEW, GebrMenuView))
+#define GEBR_MENU_VIEW_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass),  GEBR_TYPE_MENU_VIEW, GebrMenuViewClass))
+#define GEBR_IS_MENU_VIEW(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GEBR_TYPE_MENU_VIEW))
+#define GEBR_IS_MENU_VIEW_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),  GEBR_TYPE_MENU_VIEW))
+#define GEBR_MENU_VIEW_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),  GEBR_TYPE_MENU_VIEW, GebrMenuViewClass))
+
+typedef struct _GebrMenuView GebrMenuView;
+typedef struct _GebrMenuViewPriv GebrMenuViewPriv;
+typedef struct _GebrMenuViewClass GebrMenuViewClass;
+
+struct _GebrMenuView {
+	GObject parent;
+	GebrMenuViewPriv *priv;
+};
+
+struct _GebrMenuViewClass {
+	GObjectClass parent_class;
+
+	/* Signals */
+	void (*add_menu) (GebrMenuView *menuView,
+			  const gchar *menuPath);
+};
+
+enum {
+	MENU_TITLE_COLUMN = 0,
+	MENU_DESCRIPTION_COLUMN,
+	MENU_FILEPATH_COLUMN,
+	MENU_VISIBLE_COLUMN,
+	MENU_N_COLUMN
+};
+
+GType gebr_menu_view_get_type(void) G_GNUC_CONST;
+
+GebrMenuView *gebr_menu_view_new(void);
+
+/**
+ * gebr_menu_view_clear_model:
+ * @view:
+ *
+ */
+void gebr_menu_view_clear_model(GebrMenuView *view);
+
+/**
+ * gebr_menu_view_find_or_add_category:
+ * @title:
+ * @categories_hash:
+ * @view:
+ *
+ * Returns: A iter of category with @title
+ */
+GtkTreeIter gebr_menu_view_find_or_add_category(const gchar *title,
+                                                GHashTable *categories_hash,
+                                                GebrMenuView *view);
+
+/**
+ * gebr_menu_view_add_menu:
+ * @parent:
+ * @title:
+ * @desc:
+ * @file:
+ * @view:
+ *
+ */
+void gebr_menu_view_add_menu(GtkTreeIter *parent,
+                             const gchar *title,
+                             const gchar *desc,
+                             const gchar *file,
+                             GebrMenuView *view);
+/**
+ * gebr_menu_view_get_widget:
+ *
+ * Returns a widget of menu list
+ */
+GtkWidget *gebr_menu_view_get_widget(GebrMenuView *view);
+
+/**
+ * gebr_menu_view_set_focus_on_entry:
+ *
+ * Set focus on search entry
+ */
+void gebr_menu_view_set_focus_on_entry(GebrMenuView *view);
+
+/**
+ * gebr_menu_view_set_open_infobar:
+ *
+ * Verify and open the infobar when it's necessary
+ */
+void gebr_menu_view_set_open_infobar(GebrMenuView *view,  gboolean open);
+
+/**
+ * gebr_menu_view_choose_infobar_text:
+ *
+ * Verify if there is a wrong selection to add menu and set the text of infobar
+ */
+gboolean gebr_menu_view_choose_infobar_text(GebrMenuView *view,
+                                            gboolean multiple_loop,
+                                            gboolean help,
+                                            gboolean no_html);
+
+G_END_DECLS
+
+#endif /* end of include guard: __GEBR_MENU_VIEW_H__ */
+
